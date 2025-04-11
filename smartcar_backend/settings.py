b6c5b09faf3224409599f54780ced0e0cdc651a2
Leovem <misaelvediaeyz@gmail.com>
2025-04-11 19:19:03 -0400

    Subi la base de datos a railway

--- smartcar_backend/settings.py
+++ smartcar_backend/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'smartcar_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -82,7 +82,18 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'fuVAnIpyQOjIBCJOrjXjXWbGcerZECbY',
+        'HOST': 'switchback.proxy.rlwy.net',
+        'PORT': '23480',
+    }
+}
 
 
 # Password validation
